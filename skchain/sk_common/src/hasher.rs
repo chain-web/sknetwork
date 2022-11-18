37fbfc165d51698eee4368bdf966db281f7173ba
chengcheng <chengcheng@qcraft.ai>
2022-11-18 17:24:04 +0800

    [feature]base trie

---
+++ skchain/sk_common/src/hasher.rs
@@ -0,0 +1,41 @@
+//!Blake2b Hasher implementation
+
+pub mod blake2 {
+	use primitive_types::H256;
+	use hash256_std_hasher::Hash256StdHasher;
+	use hash_db::Hasher;
+
+	/// Concrete implementation of Hasher using Blake2b 256-bit hashes
+	#[derive(Debug)]
+	pub struct Blake2Hasher;
+
+	impl Hasher for Blake2Hasher {
+		type Out = H256;
+		type StdHasher = Hash256StdHasher;
+		const LENGTH: usize = 32;
+
+		fn hash(x: &[u8]) -> Self::Out {
+			sp_core_hashing::blake2_256(x).into()
+		}
+	}
+}
+
+pub mod keccak {
+	use primitive_types::H256;
+	use hash256_std_hasher::Hash256StdHasher;
+	use hash_db::Hasher;
+
+	/// Concrete implementation of Hasher using Keccak 256-bit hashes
+	#[derive(Debug)]
+	pub struct KeccakHasher;
+
+	impl Hasher for KeccakHasher {
+		type Out = H256;
+		type StdHasher = Hash256StdHasher;
+		const LENGTH: usize = 32;
+
+		fn hash(x: &[u8]) -> Self::Out {
+			sp_core_hashing::keccak_256(x).into()
+		}
+	}
+}
--- skchain/sk_common/src/lib.rs
+++ skchain/sk_common/src/lib.rs
@@ -1,5 +1,8 @@
 pub mod events;
 mod meta;
 pub mod timer;
+mod hasher;
 
 pub use meta::{account::Account, address::Address};
+pub use hasher::blake2::Blake2Hasher;
+pub use hasher::keccak::KeccakHasher;
